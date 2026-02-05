ed0b6be37287a25f0ff23ad29ec2b8635c9ebedf
Yu-ga <74749461+yuga-hashimoto@users.noreply.github.com>
2026-02-05 16:39:30 +0900

    fix: move repository configuration to settings.gradle.kts to resolve build error

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,12 +1,3 @@
-allprojects {
-    repositories {
-        google()
-        mavenCentral()
-        maven { url = uri("https://oss.sonatype.org/content/repositories/snapshots") }
-        maven { url = uri("https://jitpack.io") }
-    }
-}
-
 plugins {
     id("com.android.application") version "8.2.2" apply false
     id("org.jetbrains.kotlin.android") version "1.9.22" apply false
